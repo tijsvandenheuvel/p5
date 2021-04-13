b3329dc163457006fdde921daa57d5b32e571001
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2021-04-13 19:40:15 +0200

    attempt at seeking algorithm

--- antsimulation/ant.js
+++ antsimulation/ant.js
@@ -44,92 +44,113 @@ function Ant(id,maxspeed,maxforce,pos,vel){
                             random(-this.maxforce,this.maxforce))
     }
 
-   this.update = () =>{
+    this.update = () =>{
 
-    this.applyForce(this.randomForce());
+        this.applyForce(this.randomForce());
 
-    if(this.alive&&!this.crashed){
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
-        this.vel.limit(this.maxspeed);
-    }
-
-    if(this.pos.x>screenWidth||this.pos.x<0){
-        this.vel.x=-this.vel.x
-    }
-    if(this.pos.y>screenHeight||this.pos.y<0){
-        this.vel.y=-this.vel.y
-    }
-
-    if(frameCount%10==0){
-        this.addPosToPath();
-
-        this.path = this.path.filter((particle)=>{ 
-            return particle.age>100;
-        });
-    }
-
-    if(this.foundFood){
-        let d = dist(this.pos.x,this.pos.y,colonyObject.pos.x,colonyObject.pos.y);
-        if(d<colonyObject.size/2){
-            this.foundFood=false;
+        if(this.alive&&!this.crashed){
+            this.vel.add(this.acc);
+            this.pos.add(this.vel);
+            this.acc.mult(0);
+            this.vel.limit(this.maxspeed);
         }
-    }else{
-        let d = dist(this.pos.x,this.pos.y,foodObject.pos.x,foodObject.pos.y);
-        if(d<foodObject.size/2){
-            this.foundFood=true;
+
+        if(this.pos.x>screenWidth||this.pos.x<0){
+            this.vel.x=-this.vel.x
         }
-    }
-    // follow particles if there are any
-    if(particles.length>10){
-        //this.seek(this.getTarget())
-    }
-   }
+        if(this.pos.y>screenHeight||this.pos.y<0){
+            this.vel.y=-this.vel.y
+        }
+
+        if(frameCount%10==0){
+            this.addPosToPath();
+
+            this.path = this.path.filter((particle)=>{ 
+                return particle.age>100;
+            });
+        }
+
+        if(this.foundFood){
+            let d = dist(this.pos.x,this.pos.y,colonyObject.pos.x,colonyObject.pos.y);
+            if(d<colonyObject.size/2){
+                this.foundFood=false;
+            }
+        }else{
+            let d = dist(this.pos.x,this.pos.y,foodObject.pos.x,foodObject.pos.y);
+            if(d<foodObject.size/2){
+                this.foundFood=true;
+            }
+        }
+        // follow particles if there are any
+        if(particles.length>1){
+            this.seek(this.getTarget(),this.maxforce)
+        }
+
+        if(this.foundFood==true){
+            this.seek(colonyObject.pos,0.2)
+        }
+}
+
+   
 
     // seek closest / strongest particle in front
     // if found food follow 'home' particles
     // if not found food follow 'away' particles
-    
-    //this.seek(createVector(random(0,screenWidth),random(0,screenHeight)));
-    //this.seek(createVector(mouseX,mouseY));
+    // if not found follow home particles in reverse
+
+
     this.getTarget = () => {
 
         let mind = 100;
         let minid = 0;
         let d = 100;
 
-        // TODO: only search in front 
+        angle = this.vel.heading()
+        frontpos = createVector(this.pos.x+20*cos(angle),this.pos.y+20*sin(angle))
 
         for (let i = 0; i < particles.length; i++) {
 
-            //var direction = p5.Vector.sub(particles[i].pos, this.pos);
-
-            //console.log(direction.heading(),this.pos.heading())
-
-            //if(direction.x>0 && direction.y>0){
-                if(this.foundFood&&particles[i].toHome){
-                    d = dist(this.pos.x,this.pos.y,particles[i].pos.x,particles[i].pos.y);
+                if(!this.foundFood){
+                    d = dist(frontpos.x,frontpos.y,particles[i].pos.x,particles[i].pos.y);
+                    if(particles[i].toHome){
+                        d=d*0.5;
+                    }
                 }
-                if(!this.foundFood&&!particles[i].toHome){
-                    d = dist(this.pos.x,this.pos.y,particles[i].pos.x,particles[i].pos.y);
-                }else{
-                    d = 100
+                else{
+                    d=101;
                 }
+
+                
+                
+
+                // // you havent found food and find a to food marker
+                // if((!this.foundFood&&!particles[i].toHome)){
+                //     d = dist(frontpos.x,frontpos.y,particles[i].pos.x,particles[i].pos.y);
+                // }
+                // // you have found food and find a to home marker
+                // else if(this.foundFood&&particles[i].toHome){
+                //     d = dist(frontpos.x,frontpos.y,particles[i].pos.x,particles[i].pos.y);
+                // }
+                // // you have found food and find a to food marker
+                // else if(this.foundFood&&!particles[i].toHome){
+                //     d = 100;
+                // }
+                // else{
+                //     d = 100
+                // }
                 
                 if(d<mind){
                     mind = d;
                     minid = i;
                 }
-            //}
         }
-        if(mind<30){
+        if(mind<20){
             return particles[minid].pos
         }
         
     }
 
-    this.seek = (target) => {
+    this.seek = (target,force) => {
         if(target){
             // vector: location -> target
             var desired = p5.Vector.sub(target, this.pos);
@@ -137,7 +158,7 @@ function Ant(id,maxspeed,maxforce,pos,vel){
             desired.setMag(this.maxspeed);
 
             var steer = p5.Vector.sub(desired, this.vel);
-            steer.limit(this.maxforce/10);
+            steer.limit(force);
 
             this.applyForce(steer);
         }
