6df5e651bff42bd2d986e45643c735d8f8c5af43
tijsvandenheuvel <56115581+tijsvandenheuvel@users.noreply.github.com>
2023-02-17 15:52:32 +0100

    lokalen

--- lokalen/sketch.js
+++ lokalen/sketch.js
@@ -1,49 +1,103 @@
-let lokalen;
-let angle;;
+let sketch = function(p) {
+    let lokalen_base;
+    let lokalen_dak;
 
-function preload() {
-	//lokalen = loadModel("lokalen3.obj");
-    lokalen_base = loadModel("lokalen_base.obj");
-    lokalen_dak = loadModel("lokalen_dak.obj");
-}
+    p.preload = function(){
+        //lokalen = loadModel("lokalen3.obj");
+        lokalen_base = p.loadModel("lokalen_base.obj");
+        lokalen_dak = p.loadModel("lokalen_dak.obj");
+    }
+    p.setup = function(){
+        screenHeight = p.windowHeight - 60;
+        screenWidth = p.windowWidth * 0.999;
+    
+        p.createCanvas(screenWidth, screenHeight, p.WEBGL);
+        angle = p.PI * 1.05;
+    }
 
-function setup() {
-	screenHeight = windowHeight - 30;
-	screenWidth = windowWidth - 30;
+    p.draw = function() {
+        p.background(190, 203, 212);
+    
+        //orbitControl();
+        p.camera(0, -2 * p.height , 5 * p.height, 0, 0, 0, 0, 1, 0 );
+    
+        p.ambientLight(250, 250, 250);
+        //ambientLight(182, 102, 91);
+        p.directionalLight(255, 255, 255, 0, 0, 1);
+    
+        p.rotateY(angle);
+        angle += 0.002;
+    
+        p.push();
+        p.rotateX(p.PI / 2);
+        p.rotateZ(p.PI * 0.88);
+        p.fill('#6b8446');
+        p.noStroke();
+        p.plane(10000, 10000);
+        p.pop();
+        //sphere(50);
+    
+        p.push();
+        p.rotateX(p.PI / 2);
+        p.translate(-2750, -7500, 0);
+        p.stroke(142, 62, 51);
+        p.fill('#c7bea0');
+            //model(lokalen);
+        p.model(lokalen_base);
+        p.fill(182, 102, 91)
+        p.model(lokalen_dak);
+        p.pop();
+    }
+  };
+new p5(sketch, 'container');
 
-	createCanvas(screenWidth, screenHeight, WEBGL);
-    angle = PI * 1.05;
-}
+// let lokalen;
+// let angle;
 
-function draw() {
-	background(190, 203, 212);
+// function preload() {
+// 	//lokalen = loadModel("lokalen3.obj");
+//     lokalen_base = loadModel("lokalen_base.obj");
+//     lokalen_dak = loadModel("lokalen_dak.obj");
+// }
 
-    //orbitControl();
-    camera(0, -2 * height , 5 * height, 0, 0, 0, 0, 1, 0 );
+// function setup() {
+// 	screenHeight = windowHeight - 80;
+// 	screenWidth = windowWidth - 30;
 
-    ambientLight(250, 250, 250);
-	//ambientLight(182, 102, 91);
-	directionalLight(255, 255, 255, 0, 0, 1);
+// 	createCanvas(screenWidth, screenHeight, WEBGL);
+//     angle = PI * 1.05;
+// }
 
-	rotateY(angle);
-	angle += 0.002;
+// function draw() {
+// 	background(190, 203, 212);
 
-    push();
-        rotateX(PI / 2);
-        rotateZ(PI * 0.88);
-        fill('#6b8446');
-        noStroke();
-        plane(10000, 10000);
-    pop();
-	//sphere(50);
+//     //orbitControl();
+//     camera(0, -2 * height , 5 * height, 0, 0, 0, 0, 1, 0 );
 
-    push();
-	    rotateX(PI / 2);
-	    translate(-2750, -7500, 0);
-        stroke(142, 62, 51);
-        fill(255, 255, 255);
-	    model(lokalen_base);
-        fill(182, 102, 91)
-        model(lokalen_dak);
-    pop();
-}
+//     ambientLight(250, 250, 250);
+// 	//ambientLight(182, 102, 91);
+// 	directionalLight(255, 255, 255, 0, 0, 1);
+
+// 	rotateY(angle);
+// 	angle += 0.002;
+
+//     push();
+//         rotateX(PI / 2);
+//         rotateZ(PI * 0.88);
+//         fill('#6b8446');
+//         noStroke();
+//         plane(10000, 10000);
+//     pop();
+// 	//sphere(50);
+
+//     push();
+// 	    rotateX(PI / 2);
+// 	    translate(-2750, -7500, 0);
+//         stroke(142, 62, 51);
+//         fill(255, 255, 255);
+//         //model(lokalen);
+// 	    model(lokalen_base);
+//         fill(182, 102, 91)
+//         model(lokalen_dak);
+//     pop();
+// }
